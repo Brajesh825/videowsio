356541776789057cf6caf40724098ed28d4fb7ad
Brajesh825 <brajeshmishra825@gmail.com>
2023-08-05 20:19:37 +0530

    :p

--- index.js
+++ index.js
@@ -10,29 +10,32 @@ const rooms = {};
 
 app.use(express.static('public'));
 
+// ...
 io.on('connection', (socket) => {
-  socket.on('join-room', (roomId, userId) => {
-    socket.join(roomId);
-    socket.to(roomId).emit('user-connected', userId); // Broadcast to everyone in the room
-
-    if (rooms[roomId]) {
-      rooms[roomId].push({ id: userId, socketId: socket.id });
-    } else {
-      rooms[roomId] = [{ id: userId, socketId: socket.id }];
-    }
-
-    const existingUsers = rooms[roomId].filter(user => user.id !== userId);
-    socket.emit('existing-users', existingUsers);
-
-    socket.on('disconnect', () => {
-      const index = rooms[roomId].findIndex(user => user.socketId === socket.id);
-      if (index !== -1) {
-        rooms[roomId].splice(index, 1);
+    socket.on('join-room', (roomId, userId) => {
+      socket.join(roomId);
+      io.to(roomId).emit('user-connected', userId); // Broadcast to everyone in the room
+  
+      if (rooms[roomId]) {
+        rooms[roomId].push({ id: userId, socketId: socket.id });
+      } else {
+        rooms[roomId] = [{ id: userId, socketId: socket.id }];
       }
-      socket.to(roomId).emit('user-disconnected', userId);
+  
+      const existingUsers = rooms[roomId].filter(user => user.id !== userId);
+      socket.emit('existing-users', existingUsers);
+  
+      socket.on('disconnect', () => {
+        const index = rooms[roomId].findIndex(user => user.socketId === socket.id);
+        if (index !== -1) {
+          rooms[roomId].splice(index, 1);
+        }
+        io.to(roomId).emit('user-disconnected', userId);
+      });
     });
   });
-});
+  // ...
+  
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
